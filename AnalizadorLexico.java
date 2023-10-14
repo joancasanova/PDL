9674c2e75a50b8e4244b8814b76df5a745c9e2e2
manpere <manuel.perez.redondo@alumnos.upm.es>
2023-10-14 20:09:27 +0200

    AnLex terminado-Falta probar con ficheros y TS

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -37,8 +37,8 @@ public class AnalizadorLexico {
 	
 		//Inicializar las variabbles a devolver o actualizar
 		Token nuevo_token = null;
-		String tipo;
-		E valor;
+		String lexema = null;
+		int valor = 0;
 	        
 		//Analizar el caraceter ya leido
 		
@@ -55,20 +55,148 @@ public class AnalizadorLexico {
 			char_actual = (char) fichero.read();// Seguir leyendo
 			break;
 			
-		case 1:// Caracter es l o a
-			String lexema;
+			case 1:// Caracter es l o a
+			
+			// Empezar lexema
+			lexema = String.valueOf(char_actual);//primer valor
+			char_actual = (char) fichero.read();// leer el siguiente caracter
+			//leer caracetres hassta del,s
+			
+			while((int) char_actual != -1 || is_del(char_actual) || is_s(char_actual) || is_op(char_actual) ){
+				
+					lexema.concat(String.valueOf(char_actual));//concateno el caracter nuevo
+					char_actual = (char) fichero.read();	
+				
+			} 
+			
+			if(lexema.length() < 64) {
+			// Tras acabar se crea el token
+			nuevo_token = new Token("Nombredeltoken", lexema);
+			} else {
+				System.err.print("Maxima longitud de la cadena alcanzada");
+			}
+		/* PENDIENTE POR HACER PARA LA TABLA DE SIMBOLOS*/
+			
+			// Comprobar si el lexema pertenece a tabla palabra reservada
+				//Si pertenece genra token palabra reservada y su pos
+				//Si No pertenece 
+					//buscar en tabla de simbolos
+						//Si esta generar token tabla simbolos y su pos
+						//Si NO esta, crear en TS y devolver token
 			break;
 			
-		case 2:
+		case 2:// Caracter d
+			
+			valor = (int) char_actual;//Guardo el primer digiton leido
+			char_actual = (char) fichero.read();// leo el siguiente caracter
+			
+			//leer hasta que char_actual != d
+			
+			while((int) char_actual != -1 || is_d(char_actual)) {
+				
+				valor = 10*valor + (int) char_actual;
+				char_actual = (char) fichero.read();
+				
+			}
+			
+			if(valor < 32767) {
+			// Tras acabar se crea el token
+						nuevo_token = new Token("cte_entera", String.valueOf(valor) );
+			} else {
+				System.err.println("Se ha superado el valor maximo de la representacion");
+			}
+			
 			break ;
 			
 		case 3:
+			
+			char_actual = (char) fichero.read();// leo el siguiente caracter
+			
+			if(char_actual == '/') {//se han leido dos \\
+				
+				char_actual = (char) fichero.read();// leo el siguiente caracter
+				
+				while( (int) char_actual != -1 && !is_f(char_actual)) { // Cualquier cosa menos f
+					char_actual = (char) fichero.read();//Continuo leyendo el comentairo hastas fin de linea o fichero
+				}
+				
+				//Deja leido el ultimo caracter para la siguiente lectura, no hay que leer
+			} else {
+				System.err.println("Error lexico, solo se ha leido / en lugar de //(no triene token) ");
+			}
+			
 			break;
 			
 		case 4:
+			
+			char_actual = (char) fichero.read();// leo el siguiente caracter
+			lexema = String.valueOf(char_actual);//primer valor
+			
+			while(char_actual != '"') {// Continua leyendo la cadena hasta encontrar ", pasra acabar
+				
+				if((int) char_actual != -1	||  is_f(char_actual)){// Si se lee fin de linea o fichero antesde acabar la cadena da error
+					System.err.println("Error la cadena nose ha cerrado con \"");
+					break;
+				}
+				
+				lexema.concat(String.valueOf(char_actual));//concateno el caracter nuevo
+				char_actual = (char) fichero.read();
+			}
+			
+			if(lexema.length() < 64) {
+				// Tras acabar se crea el token
+				nuevo_token = new Token("Nombredeltoken", lexema);
+				} else {
+					System.err.print("Maxima longitud de la cadena alcanzada");
+				}
+			
 			break;
 			
 		case 5:
+			
+			if(char_actual == ',') valor = 1;
+			if(char_actual == ';') valor = 2;
+			if(char_actual == '(') valor = 3;
+			if(char_actual == ')') valor = 4;
+			if(char_actual == '{') valor = 5;
+			if(char_actual == '}') valor = 6;
+			
+			nuevo_token = new Token("Simbolo",String.valueOf(valor));
+			char_actual = (char) fichero.read();// leo el siguiente caracter para el siguiente token
+			
+			break;
+			
+		case 6:
+			
+			if(char_actual == '!') {
+				nuevo_token = new Token("operador", "2");
+				char_actual = (char) fichero.read();// leo el siguiente caracter para el siguiente token
+			} else {
+				
+				if(char_actual == '+') {//Primer simbolo es +
+					char_actual = (char) fichero.read();// leo el siguiente caracter para comprobar que op es
+					
+					if(char_actual == '=') {
+						nuevo_token = new Token("asignacion", "1");// Token asignador +=
+						char_actual = (char) fichero.read();// leo el siguiente caracter para el siguiente token
+					} else {
+						//El caracter es unicamente +
+						nuevo_token = new Token("operador", "1");// Token operador +
+					}
+					
+				} else {
+					char_actual = (char) fichero.read();// leo el siguiente caracter para comprobar que op es
+					
+					if(char_actual == '=') {
+						nuevo_token = new Token("operador", "3");// Token asignador +=
+						char_actual = (char) fichero.read();// leo el siguiente caracter para el siguiente token
+					} else {
+						//El caracter es unicamente +
+						nuevo_token = new Token("asignacion", "2");// Token operador +
+					}
+				}
+			}
+			
 			break;
 			
 			default:
@@ -111,19 +239,26 @@ public class AnalizadorLexico {
 		if((int)caracter == -1) return false; // Si es -1 entonces acaba el fichero
 		return caracter == '\n'; // Fin de linea
 	}
+
+	private boolean is_op(char caracter) {
+		if((int)caracter == -1) return false; // Si es -1 entonces acaba el fichero
+		return caracter == '+' || caracter == '=' || caracter == '!';
+	}
 	
 	private int analizarchar(char caracter) {
 		if((int) caracter == -1) return -1; // Solo si es el caracter fin de fichero
 		
+		// Todas las ramas del automata cubiertas
 		if(is_del(caracter)) return 0;
 		if(is_l(caracter) || is_a(caracter)) return 1;
 		if(is_d(caracter)) return 2;
-		if(is_s(caracter)) return 3;
-		if(is_f(caracter)) return 4;
-		
-		return 5;//Caso para simbolos de los operadores aritmeicos y logicos
+		if(caracter == '/') return 3;
+		if(caracter == '"') return 4;
+		if(is_s(caracter)) return 5;
+		if(is_op(caracter)) return 6;
+
+		return -1;
 	}
-	
 		
 
 	
