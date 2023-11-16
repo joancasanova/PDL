dd08356927974d345f7ccd2ecbc1aeea31ca8553
Joan Casanova Ferrer <juancasanovaferrer@gmail.com>
2023-11-16 23:38:50 +0100

    TablaAnalisis.java completa y funcional

--- AnalizadorSintactico.java
+++ AnalizadorSintactico.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import javafx.util;
 
 // Enumeración para representar las acciones posibles
 enum Accion {
@@ -19,72 +18,4 @@ class EntradaTabla {
 
 // Clase para el analizador sintáctico
 public class AnalizadorSintactico {
-
-    private Stack<Integer> pila; // Pila de estados y terminales
-    
-    private Map<Pair<Integer, Character>, EntradaTabla> tablaAnalisis; // Tabla de análisis LR
-    private List<String> tokens; // Lista de tokens de entrada -> No hace falta, el llama al analizador lexico para cada token que ingresa a la pila
-
-    public AnalizadorSintactico() {
-        pila = new Stack<>();
-        inicializarTablaAnalisis();
-    }
-
-    private void inicializarTablaAnalisis() {
-        
-        //delimitador ":" -> Para diferenciar los parámetros del fichero
-        //      _____&:____  ______...
-        //   1: |  D : 6:  |-|      ...
-        //   2: |  X : 1:  |-|      ...
-
-        // Formato fichero -> Estado:Simbolo:Accion:Digito
-        // ejemplo:
-        // 0:+:Desplazar:7
-        // 0:+:Reducir:3
-        // ...
-
-        // leer ficherox2(leer estado y simbolo)
-        // asignar valores al pair(clave del hashmap)
-        // leer ficherox2 accion y digito
-
-        // Crear par clave(pair<estado,simbolo>) y valor(pair<accion,digito>)(Pongo pair pero es cualquier mierda similar)
-        //repetir hata find e fichero
-
-        //tabla creada--yupii
-    }
-
-    public void analizar() {
-        pila.push(0); // Estado inicial
-        int indiceToken = 0;
-
-        while (indiceToken < tokens.size()) {
-            String tokenActual = tokens.get(indiceToken);
-            int estadoActual = pila.peek();
-
-            EntradaTabla entrada = tablaAnalisis.getOrDefault(estadoActual, new HashMap<>()).get(tokenActual);
-
-            if (entrada == null) {
-                System.out.println("Error sintáctico");
-                return;
-            }
-
-            switch (entrada.accion) {
-                case DESPLAZAR:
-                    pila.push(Integer.parseInt(tokenActual));
-                    pila.push(entrada.numero);
-                    indiceToken++;
-                    break;
-                case REDUCIR:
-                    // Implementar lógica de reducción según la regla especificada en entrada.numero
-                    break;
-                case ACEPTAR:
-                    System.out.println("Análisis sintáctico completado exitosamente");
-                    return;
-                case ERROR:
-                default:
-                    System.out.println("Error sintáctico");
-                    return;
-            }
-        }
-    }
 }
--- TablaAnalisis.java
+++ TablaAnalisis.java
@@ -4,81 +4,154 @@ import java.util.*;
 public class TablaAnalisis {
     private Map<Integer, Map<String, String>> actionTable;
     private Map<Integer, Map<String, Integer>> gotoTable;
-    private List<String> terminals;
-    private List<String> nonTerminals;
+    private Set<String> terminals;
+    private Set<String> nonTerminals;
 
     public TablaAnalisis() {
         actionTable = new HashMap<>();
         gotoTable = new HashMap<>();
-        terminals = new ArrayList<>();
-        nonTerminals = new ArrayList<>();
+        terminals = new HashSet<>();
+        nonTerminals = new HashSet<>();
     }
 
-public void generateTable(String filePath) {
-    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("Terminals")) {
-                processTerminals(reader);
-            } else if (line.startsWith("Nonterminals")) {
-                processNonTerminals(reader);
-            } else if (line.startsWith("State")) {
-                processState(reader, line);
+    public void generateTable(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("conflicts") || line.contains("unused") || line.contains("useless")) {
+                    continue;
+                }
+
+                if (line.startsWith("Terminals")) {
+                    processTerminals(reader);
+                } else if (line.startsWith("Nonterminals")) {
+                    processNonTerminals(reader);
+                } else if (line.startsWith("State")) {
+                    processState(reader, line);
+                }
             }
-            // Continuar con otras secciones si las hay
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-
-private void processTerminals(BufferedReader reader) throws IOException {
-    String line;
-    while (!(line = reader.readLine()).isEmpty()) {
-        terminals.add(line.trim());
-    }
-}
-
-private void processNonTerminals(BufferedReader reader) throws IOException {
-    String line;
-    while (!(line = reader.readLine()).isEmpty()) {
-        nonTerminals.add(line.trim());
-    }
-}
-
-private void processState(BufferedReader reader, String firstLine) throws IOException {
-    // Extraemos el número del estado de la primera línea
-    int stateNumber = Integer.parseInt(firstLine.split(" ")[1]);
-    Map<String, String> actionMap = new HashMap<>();
-    Map<String, Integer> gotoMap = new HashMap<>();
-
-    String line;
-    while (!(line = reader.readLine()).trim().isEmpty()) {
-        // Divide la línea en partes para analizarlas
-        String[] parts = line.split("\\s+");
-        String symbol = parts[1]; // El símbolo es la segunda parte (shift, reduce, etc.)
-
-        if (symbol.equals("$default")) {
-            // Maneja el caso especial del $default
-            String action = parts[3];
-            actionMap.put("$default", action);
-        } else if (parts.length >= 5 && (parts[3].equals("shift,") || parts[3].equals("shift"))) {
-            // Si es una acción de desplazamiento
-            int goToState = Integer.parseInt(parts[parts.length - 1]);
-            actionMap.put(symbol, "s" + goToState);
-        } else if (parts.length >= 3 && (parts[2].equals("reduce") || parts[2].equals("reduce,"))) {
-            // Si es una acción de reducción
-            int ruleNumber = Integer.parseInt(parts[parts.length - 1].replaceAll("[()]", ""));
-            actionMap.put(symbol, "r" + ruleNumber);
-        } else {
-            // Para las entradas de la tabla GOTO
-            int goToState = Integer.parseInt(parts[parts.length - 1]);
-            gotoMap.put(symbol, goToState);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    // Agregar los mapas a las tablas generales
-    actionTable.put(stateNumber, actionMap);
-    gotoTable.put(stateNumber, gotoMap);
-}
+    private void processTerminals(BufferedReader reader) throws IOException {
+        String line;
+        Integer contador_lineas_vacias = 0;
+        while (contador_lineas_vacias < 2) {
+            line = reader.readLine();
+            if (line.isEmpty()) {
+                contador_lineas_vacias++;
+                continue;
+            }
+            terminals.add(line.split("\\s+")[1]);
+        }
+    }
+
+    private void processNonTerminals(BufferedReader reader) throws IOException {
+        String line;
+        Integer contador_lineas_vacias = 0;
+        while (contador_lineas_vacias < 2) {
+            line = reader.readLine();
+            if (line.isEmpty()) {
+                contador_lineas_vacias++;
+                continue;
+            } else if (line.contains("left") || line.contains("right")) {
+                continue;
+            }
+
+            nonTerminals.add(line.split("\\s+")[1]);
+        }
+    }
+
+    private void processState(BufferedReader reader, String firstLine) throws IOException {
+        int stateNumber = Integer.parseInt(firstLine.split("\\s+")[1]);
+        Map<String, String> actionMap = new HashMap<>();
+        Map<String, Integer> gotoMap = new HashMap<>();
+
+        String line;
+        Integer contador_lineas_vacias = 0;
+        while (contador_lineas_vacias < 5) {
+            line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            if (line.isEmpty()) {
+                contador_lineas_vacias++;
+                continue;
+            }
+            
+            String[] parts = line.trim().split("\\s+");
+            String symbol = parts[0];
+            try {
+                if (parts[1].equals("shift,")) {
+                    int goToState = Integer.parseInt(parts[6]);
+                    actionMap.put(symbol, "s" + goToState);
+                } else if (parts[1].equals("reduce")) {
+                    int ruleNumber = Integer.parseInt(parts[4].replaceAll("[^0-9]", ""));
+                    actionMap.put(symbol, "r" + ruleNumber);
+                } else if (parts[1].equals("go")) {
+                    int goToState = Integer.parseInt(parts[4]);
+                    gotoMap.put(symbol, goToState);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Skipping invalid line: " + line);
+            }
+        }
+
+        actionTable.put(stateNumber, actionMap);
+        gotoTable.put(stateNumber, gotoMap);
+    }
+
+    private void printActionTable() {
+        System.out.println("-------------");
+        System.out.println("-------------");
+        System.out.println("Action Table:");
+        for (Map.Entry<Integer, Map<String, String>> entry : actionTable.entrySet()) {
+            System.out.println("State " + entry.getKey() + ":");
+            for (Map.Entry<String, String> action : entry.getValue().entrySet()) {
+                System.out.println("  " + action.getKey() + " -> " + action.getValue());
+            }
+        }
+    }
+
+    private void printGotoTable() {
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("Goto Table:");
+        for (Map.Entry<Integer, Map<String, Integer>> entry : gotoTable.entrySet()) {
+            System.out.println("State " + entry.getKey() + ":");
+            for (Map.Entry<String, Integer> goTo : entry.getValue().entrySet()) {
+                System.out.println("  " + goTo.getKey() + " -> " + goTo.getValue());
+            }
+        }
+    }
+
+    private void printTerminals() {
+        System.out.println("----------");
+        System.out.println("----------");
+        System.out.println("Terminals:");
+        for (String terminal : terminals) {
+            System.out.println("  " + terminal);
+        }
+    }
+
+    private void printNonTerminals() {
+        System.out.println("--------------");
+        System.out.println("--------------");
+        System.out.println("Non-Terminals:");
+        for (String nonTerminal : nonTerminals) {
+            System.out.println("  " + nonTerminal);
+        }
+    }
+
+    public static void main(String[] args) {
+        TablaAnalisis generator = new TablaAnalisis();
+        String filePath = "gramatica.txt";
+        generator.generateTable(filePath);
+        generator.printActionTable();
+        generator.printGotoTable();
+        generator.printTerminals();
+        generator.printNonTerminals();
+    }
 }
